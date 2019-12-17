b18d694ee39ee6cd3e31626e425dbf819bc2a387
cecimermoz <cecii.mermoz@gmail.com>
2019-12-16 21:02:00 -0300

    mostre palabras

--- palabra.js
+++ palabra.js
@@ -2,7 +2,7 @@ class Palabra{
     constructor(estado, palabra){
         this.estado = estado;
         this.palabra = palabra;
-        this.puntos = this.palabra.length();
+        this.puntos = this.palabra.length;
         this.tiempo = this.puntos*2;
     }
     estadoPalabra(){}
