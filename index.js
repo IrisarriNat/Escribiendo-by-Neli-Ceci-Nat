3a5d44ed471fe54493524839b5c98e4a0995788a
cecimermoz <cecii.mermoz@gmail.com>
2019-12-18 22:13:00 -0300

    funcion validar

--- index.js
+++ index.js
@@ -25,23 +25,57 @@ var listadoPalabras = [ "Compromiso", "Quince", "Profesion", "Colocar", "Boton",
 "Cepillar", "Reloj", "Pelicula", "Torpe", "Parasol", "Ayuda", "Saltar", "Honrado", "Africano", "Musulman", 
 "Cruz", "Urgencia",];
 
-var palabra1 = new Palabra(false, listadoPalabras[0]);
-var jugador1 = new Jugador(0, 5);
-
-// agarrar palabra random
-// mostrar palabra q se eligio
-// mostrar puntos de jugador
-// mostrar vidas
-
-//$(".palabra h2").html(palabra1.palabra);
-
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
-var numPalabra = getRandomInt(listadoPalabras.length);
-var palabra1 = new Palabra(false, listadoPalabras[numPalabra]);
-var jugador1 = new Jugador(0, 5);
+
+let numPalabra = getRandomInt(listadoPalabras.length);
+let palabra1 = new Palabra(false, listadoPalabras[numPalabra]);
+let jugador1 = new Jugador(0, 5);
 $(".palabra h2").html(palabra1.palabra);
 $(".puntos").html(jugador1.acumuladorPuntos);
 $(".vidas").html(jugador1.cantidadVidas);
+
+function procesarInput(){
+    let input1 = $(".inputPalabra").val();
+    return input1;
+}
+
+function conocerPalabra(Pal){
+    return Pal.palabra;
+}
+
+function conocerEstadoPalabra(Pal){
+    return Pal.estado;
+}
+
+function validar(){
+    let user = procesarInput();
+    let palabra = conocerPalabra(palabra1);
+    let estado = conocerEstadoPalabra(palabra1);
+    if(!estado){
+        alert("Perdiste!");
+    } else if(estado && palabra==user){
+        alert("Ganaste!");
+    } else if(estado && palabra !== user){
+        alert("No pasa nada");
+    }
+}
+
+
+// 1- variable que crea la palabra en estado false
+// 2- variable que la muestra por pantalla y de estado true --> $.html 
+// 3- setTimeout --> que a esa variable, le aplique un .hide() y cambie el estado a false
+
+/* function crearPalabra(){
+    let palabra1 = new Palabra(false, listadoPalabras[numPalabra]);
+    return palabra1;
+}
+function mostrarEnPantalla(){
+    let asd = crearPalabra();
+    $(".palabra h2").html(asd.palabra);
+
+    setTimeout(function(){asd.hide()}, asd.tiempo) --
+} 
+mostrarEnPantalla();
+*/
